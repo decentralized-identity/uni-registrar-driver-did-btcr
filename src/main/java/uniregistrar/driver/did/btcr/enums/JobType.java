3c35fbf9076d75c47da057f32eeff99c345a1a86
Cihan Saglam <cihan.saglam@danubetech.com>
2020-07-14 07:36:48 +0200

    add FundingType and JobType enum classes

---
+++ src/main/java/uniregistrar/driver/did/btcr/enums/JobType.java
@@ -0,0 +1,25 @@
+package uniregistrar.driver.did.btcr.enums;
+
+import com.google.common.base.Preconditions;
+
+/**
+ * The Enum class to differentiate DidBtcrJobs
+ * @see uniregistrar.driver.did.btcr.DidBtcrJob
+ */
+public enum JobType {
+    REGISTER, UPDATE, DEACTIVATE;
+
+    public static JobType fromString(String jobType) {
+        Preconditions.checkNotNull(jobType, "Job type type is null");
+        switch (jobType.toUpperCase()) {
+            case "REGISTER":
+                return REGISTER;
+            case "UPDATE":
+                return UPDATE;
+            case "DEACTIVATE":
+                return DEACTIVATE;
+            default:
+                throw new IllegalArgumentException("Invalid job type!");
+        }
+    }
+}
