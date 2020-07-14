d3033dbc0df6bf25b1a565785bbcbc4d016c0028
Cihan Saglam <cihan.saglam@danubetech.com>
2020-07-14 22:50:31 +0200

    fix docker entry

--- docker/run-driver-did-btcr.sh
+++ docker/run-driver-did-btcr.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-mkdir ${uniregistrar_driver_did_btcr_basePath}
+mkdir -p ${uniregistrar_driver_did_btcr_basePath}
+mkdir -p ${uniregistrar_driver_did_btcr_walletPathMainnet}
+mkdir -p ${uniregistrar_driver_did_btcr_walletPathTestnet}
+mkdir -p ${uniregistrar_driver_did_btcr_walletPathRegtestnet}
 
 cd /opt/driver-did-btcr/
 mvn jetty:run -P war -Dorg.eclipse.jetty.annotations.maxWait=240
