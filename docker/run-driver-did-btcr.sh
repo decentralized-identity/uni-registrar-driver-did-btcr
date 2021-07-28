4a3735ac4c2f91f845e7a521a02f25f6516a120d
Bernhard Fuchs <bernhard.fuchs@danubetech.com>
2021-07-28 16:52:14 +0200

    ci: multi-stage docker build

--- docker/run-driver-did-btcr.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-mkdir -p ${uniregistrar_driver_did_btcr_basePath}
-mkdir -p ${uniregistrar_driver_did_btcr_walletPathMainnet}
-mkdir -p ${uniregistrar_driver_did_btcr_walletPathTestnet}
-mkdir -p ${uniregistrar_driver_did_btcr_walletPathRegtestnet}
-
-cd /opt/driver-did-btcr/
-mvn jetty:run -P war -Dorg.eclipse.jetty.annotations.maxWait=240
